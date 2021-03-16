e598a4b117ee93b555642a599868f722bf0b1850
VKirijanova <v.kirijanova@gmail.com>
2021-03-16 18:31:01 +0000

    css changes

--- end.js
+++ end.js
@@ -32,3 +32,22 @@ saveHighScore = (e) => {
   localStorage.setItem("highScores", JSON.stringify(highScores));
   window.location.assign("/");
 };
+
+/* Sending emails */
+function sendMail(contactForm) {
+  emailjs
+    .send("service_edl6rt5", "template_t45mpqs", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", responce);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+  return false;
+}
